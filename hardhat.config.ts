8f36dac4a0ed7ac58e6693776a9087b37fb248e7
Lawrence Ezealor <ezealor20@gmail.com>
2024-02-26 19:33:21 +0100

    updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,14 +14,14 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     hardhat: {
-      // forking: {
-      //   url: ALCHEMY_MAINNET_API_KEY_URL,
-      // }
+      forking: {
+        url: ALCHEMY_MAINNET_API_KEY_URL,
+      }
     },
-    sepolia: {
-      url: ALCHEMY_SEPOLIA_API_KEY_URL,
-      accounts: [ACCOUNT_PRIVATE_KEY],
-    },
+    // sepolia: {
+    //   url: ALCHEMY_SEPOLIA_API_KEY_URL,
+    //   accounts: [ACCOUNT_PRIVATE_KEY],
+    // },
     // mumbai: {
     //   url: ALCHEMY_MUMBAI_API_KEY_URL,
     //   accounts: [ACCOUNT_PRIVATE_KEY],
--- scripts/swapExactTokensForTokens.ts
+++ scripts/swapExactTokensForTokens.ts
@@ -2,36 +2,27 @@ import { ethers } from "hardhat";
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
 const main = async () => {
-  //USDC contract Address
-  const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+    const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+    const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const wethAdress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
-  //DAI contract Address
-  const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 
-  //WETH contract Address
-  const wethAdress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    const USDCHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
+    await helpers.impersonateAccount(USDCHolder);
+    const impersonatedSigner = await ethers.getSigner(USDCHolder);
 
-  //UNISWAP Router contract Address
-  const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const dAmountA = ethers.parseUnits("10", 6);
+    const dAmountB = ethers.parseUnits("10", 18);
 
-  // Impersonated account
-  const AssetHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
+    const amountOut = ethers.parseUnits("2000", 6);
 
-  await helpers.impersonateAccount(AssetHolder);
-  const impersonatedSigner = await ethers.getSigner(AssetHolder);
+    const USDC = await ethers.getContractAt("IERC20", USDCAddress);
+    const DAI = await ethers.getContractAt("IERC20", DAIAddress);
+    const WETH = await ethers.getContractAt("IERC20", wethAdress);
 
-  // This gets contract for interaction - (tokens)
-  const USDC = await ethers.getContractAt("IERC20", USDCAddress);
-  const DAI = await ethers.getContractAt("IERC20", DAIAddress);
-  const WETH = await ethers.getContractAt("IERC20", wethAdress);
-
-  // This gets contract for interaction - (router)
-  const ROUTER = await ethers.getContractAt("IUniSwap", UNIRouter);
-
-  const amountOut = ethers.parseUnits("5000", 6);
-  
-
-  // Approve tokens
+    const ROUTER = await ethers.getContractAt("IUniswap", UNIRouter);
+    
   const approve1 = await USDC.connect(impersonatedSigner).approve(
     UNIRouter,
     amountOut
@@ -39,34 +30,29 @@ const main = async () => {
 
   await approve1.wait();
 
-  //balance before swap
-  const usdcBalBeforeSwap = await USDC.balanceOf(impersonatedSigner.address);
-  const daiBalBeforeSwap = await DAI.balanceOf(impersonatedSigner.address);
+    const usdcBal = await USDC.balanceOf(impersonatedSigner.address);
+    const daiBal = await DAI.balanceOf(impersonatedSigner.address);
 
-  console.log(
-    "USDC Balance before Swap: ",
-    ethers.formatUnits(usdcBalBeforeSwap, 6)
-  );
-  console.log(
-    "DAI Balance before Swap: ",
-    ethers.formatUnits(daiBalBeforeSwap, 18)
-  );
+    console.log("USDC Balance:", usdcBal);
+    console.log("Dal Balance:", daiBal);
 
-  console.log(
-    "--------------------"
-  );
+    const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
 
-  const deadline = Math.floor(Date.now() / 1000) + 60 * 10;
-
-  const swapTx = await ROUTER.connect(
-    impersonatedSigner
-  ).swapExactTokensForTokensSupportingFeeOnTransferTokens(
+  const swapTx = await ROUTER.connect(impersonatedSigner).swapExactTokensForTokens(
     amountOut,
     0,
     [USDCAddress, DAIAddress],
     impersonatedSigner.address,
     deadline
   );
+    
+    // const swapTx = await ROUTER.swapTokensForExactETH(
+    //     amountOut,
+    //     amountIn,
+    //     [USDCAddress, wethAdress],
+    //     impersonatedSigner.address,
+    //     deadline
+    // );
 
   await swapTx.wait();
 
